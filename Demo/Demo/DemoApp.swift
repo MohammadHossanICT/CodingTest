c08087a62c939fdd4bdb336196a0bf591ebe594e
MohammadHossan <mohammadrasel83@outlook.com>
2024-04-01 12:46:31 +0100

    The project is created.

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by MohammadHossan on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
