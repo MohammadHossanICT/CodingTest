fe1dc93e6679d9711c361b3c2060ff3b71f08980
MohammadHossan <mohammadrasel83@outlook.com>
2024-04-02 15:36:00 +0100

    Refresh control code is added

--- Demo/Demo/View/EmployeeListView.swift
+++ Demo/Demo/View/EmployeeListView.swift
@@ -38,6 +38,9 @@ struct EmployeeListView: View {
         .task {
             await getDataFromAPI()
         }
+        .refreshable {
+            await getDataFromAPI()
+        }
     }
     
     // MARK: - Making API call call URL .
